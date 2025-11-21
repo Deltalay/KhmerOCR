ce6404251bd124641770ea35e15ecd44ce37c8af
Deltalay <paylayhok168@gmail.com>
2025-11-21 09:23:20 +0700

    Add Vocab

--- src/train.py
+++ src/train.py
@@ -1,17 +1,19 @@
-import torch
-from torch.utils.data import DataLoader
-import torch.nn as nn
-import torch.optim as optim
 import os
 
-from model import PretrainedOCR
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+
 from dataloader import DataloaderProj
 from load import load_labels, split_dataset
+from model import PretrainedOCR
 
 device = "cuda"
 from tokenizer import Tokenizer
 
 tokenizer = Tokenizer()
+
 
 def collate_fn_ctc(batch):
     images, labels = zip(*batch)
@@ -20,16 +22,23 @@ def collate_fn_ctc(batch):
     labels_concat = torch.cat(labels)
     return images, labels_concat, label_lengths
 
+
 image_paths, labels = load_labels("labels.txt")
-(train_img, train_labels), (val_img, val_labels) = split_dataset(image_paths, labels, val_ratio=0.2)
+(train_img, train_labels), (val_img, val_labels) = split_dataset(
+    image_paths, labels, val_ratio=0.2
+)
 
 train_dataset = DataloaderProj(train_img, train_labels, tokenizer, img_size=(224, 224))
 val_dataset = DataloaderProj(val_img, val_labels, tokenizer, img_size=(224, 224))
 
-train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True, collate_fn=collate_fn_ctc)
-val_loader = DataLoader(val_dataset, batch_size=16, shuffle=False, collate_fn=collate_fn_ctc)
+train_loader = DataLoader(
+    train_dataset, batch_size=16, shuffle=True, collate_fn=collate_fn_ctc
+)
+val_loader = DataLoader(
+    val_dataset, batch_size=16, shuffle=False, collate_fn=collate_fn_ctc
+)
 
-vocab_size = len(tokenizer)
+vocab_size = tokenizer.get_size()
 model = PretrainedOCR(num_classes=vocab_size).to(device)
 
 ctc_loss = nn.CTCLoss(blank=tokenizer.blank_id(), zero_infinity=True)
@@ -50,7 +59,9 @@ for epoch in range(num_epochs):
 
         outputs = model(images)
         outputs = outputs.permute(1, 0, 2)
-        input_lengths = torch.full(size=(images.size(0),), fill_value=outputs.size(0), dtype=torch.long).to(device)
+        input_lengths = torch.full(
+            size=(images.size(0),), fill_value=outputs.size(0), dtype=torch.long
+        ).to(device)
 
         loss = ctc_loss(outputs, targets, input_lengths, target_lengths)
 
@@ -58,7 +69,9 @@ for epoch in range(num_epochs):
         loss.backward()
         optimizer.step()
         if (i + 1) % 100 == 0:
-            print(f"[Step {i + 1}/{len(train_loader)}] Training Loss: {loss.item():.4f}")
+            print(
+                f"[Step {i + 1}/{len(train_loader)}] Training Loss: {loss.item():.4f}"
+            )
         i = i + 1
         train_loss += loss.item()
 
@@ -74,21 +87,28 @@ for epoch in range(num_epochs):
 
             outputs = model(images)
             outputs = outputs.permute(1, 0, 2)
-            input_lengths = torch.full(size=(images.size(0),), fill_value=outputs.size(0), dtype=torch.long).to(device)
+            input_lengths = torch.full(
+                size=(images.size(0),), fill_value=outputs.size(0), dtype=torch.long
+            ).to(device)
 
             loss = ctc_loss(outputs, targets, input_lengths, target_lengths)
             val_loss += loss.item()
 
     avg_val_loss = val_loss / len(val_loader)
 
-    print(f"Epoch [{epoch+1}/{num_epochs}] - Train Loss: {avg_train_loss:.4f}, Val Loss: {avg_val_loss:.4f}")
- 
-    checkpoint_path = os.path.join(save_dir, f"epoch_{epoch+1}.pt")
-    torch.save({
-        'epoch': epoch+1,
-        'model_state_dict': model.state_dict(),
-        'optimizer_state_dict': optimizer.state_dict(),
-        'train_loss': avg_train_loss,
-        'val_loss': avg_val_loss,
-    }, checkpoint_path)
+    print(
+        f"Epoch [{epoch + 1}/{num_epochs}] - Train Loss: {avg_train_loss:.4f}, Val Loss: {avg_val_loss:.4f}"
+    )
+
+    checkpoint_path = os.path.join(save_dir, f"epoch_{epoch + 1}.pt")
+    torch.save(
+        {
+            "epoch": epoch + 1,
+            "model_state_dict": model.state_dict(),
+            "optimizer_state_dict": optimizer.state_dict(),
+            "train_loss": avg_train_loss,
+            "val_loss": avg_val_loss,
+        },
+        checkpoint_path,
+    )
     print(f"Saved checkpoint: {checkpoint_path}")
